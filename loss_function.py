a898d6c0ab7c8a84f3c98e0a3f3fd915dfa5acb6
junqiangchen <1207173174@qq.com>
2019-03-22 16:47:11 +0800

    Add files via upload
    add surface loss implement

--- loss_function.py
+++ loss_function.py
@@ -32,12 +32,11 @@ def tversky_loss_3d(Y_gt, Y_pred):
     p1 = ones - Y_pred
     g0 = Y_gt
     g1 = ones - Y_gt
-    num = tf.reduce_sum(p0 * g0, axis=(0, 1, 2, 3))
-    den = num + alpha * tf.reduce_sum(p0 * g1, axis=(0, 1, 2, 3)) + \
-          beta * tf.reduce_sum(p1 * g0, axis=(0, 1, 2, 3)) + smooth
-    T = tf.reduce_sum(num / den)
-    Ncl = tf.cast(tf.shape(Y_gt)[-1], 'float32')
-    loss = Ncl - T
+    num = tf.reduce_sum(p0 * g0, axis=[1, 2, 3])
+    den = num + alpha * tf.reduce_sum(p0 * g1, axis=[1, 2, 3]) + \
+          beta * tf.reduce_sum(p1 * g0, axis=[1, 2, 3]) + smooth
+    tversky = tf.reduce_sum(num / den, axis=1)
+    loss = tf.reduce_mean(1 - tversky)
     return loss
 
 
@@ -50,50 +49,76 @@ def tversky_loss_2d(Y_gt, Y_pred):
     p1 = ones - Y_pred
     g0 = Y_gt
     g1 = ones - Y_gt
-    num = tf.reduce_sum(p0 * g0, axis=(0, 1, 2))
-    den = num + alpha * tf.reduce_sum(p0 * g1, axis=(0, 1, 2)) + \
-          beta * tf.reduce_sum(p1 * g0, axis=(0, 1, 2)) + smooth
-    T = tf.reduce_sum(num / den)
-    Ncl = tf.cast(tf.shape(Y_gt)[-1], 'float32')
-    loss = Ncl - T
+    num = tf.reduce_sum(p0 * g0, axis=[1, 2])
+    den = num + alpha * tf.reduce_sum(p0 * g1, axis=[1, 2]) + \
+          beta * tf.reduce_sum(p1 * g0, axis=[1, 2]) + smooth
+    tversky = tf.reduce_sum(num / den, axis=1)
+    loss = tf.reduce_mean(1 - tversky)
     return loss
 
 
 def generalised_dice_loss_3d(Y_gt, Y_pred):
     smooth = 1e-5
-    Ncl = tf.shape(Y_pred)[-1]
-    w = tf.zeros(shape=(Ncl,))
-    w = tf.reduce_sum(Y_gt, axis=(0, 1, 2, 3))
+    w = tf.reduce_sum(Y_gt, axis=[1, 2, 3])
     w = 1 / (w ** 2 + smooth)
 
     numerator = Y_gt * Y_pred
-    numerator = w * tf.reduce_sum(numerator, axis=(0, 1, 2, 3, 4))
-    numerator = tf.reduce_sum(numerator)
+    numerator = w * tf.reduce_sum(numerator, axis=[1, 2, 3])
+    numerator = tf.reduce_sum(numerator, axis=1)
 
     denominator = Y_pred + Y_gt
-    denominator = w * tf.reduce_sum(denominator, axis=(0, 1, 2, 3, 4))
-    denominator = tf.reduce_sum(denominator)
+    denominator = w * tf.reduce_sum(denominator, axis=[1, 2, 3])
+    denominator = tf.reduce_sum(denominator, axis=1)
 
     gen_dice_coef = 2 * numerator / (denominator + smooth)
-    loss = 1 - gen_dice_coef
+    loss = tf.reduce_mean(1 - gen_dice_coef)
+    return loss
+
+
+def generalised_dice_loss_2d_ein(Y_gt, Y_pred):
+    Y_gt = tf.cast(Y_gt, 'float32')
+    Y_pred = tf.cast(Y_pred, 'float32')
+    w = tf.einsum("bwhc->bc", Y_gt)
+    w = 1 / ((w + 1e-10) ** 2)
+    intersection = w * tf.einsum("bwhc,bwhc->bc", Y_pred, Y_gt)
+    union = w * (tf.einsum("bwhc->bc", Y_pred) + tf.einsum("bwhc->bc", Y_gt))
+
+    divided = 1 - 2 * (tf.einsum("bc->b", intersection) + 1e-10) / (tf.einsum("bc->b", union) + 1e-10)
+
+    loss = tf.reduce_mean(divided)
     return loss
 
 
 def generalised_dice_loss_2d(Y_gt, Y_pred):
     smooth = 1e-5
-    Ncl = tf.shape(Y_pred)[-1]
-    w = tf.zeros(shape=(Ncl,))
-    w = tf.reduce_sum(Y_gt, axis=(0, 1, 2))
+    w = tf.reduce_sum(Y_gt, axis=[1, 2])
     w = 1 / (w ** 2 + smooth)
 
     numerator = Y_gt * Y_pred
-    numerator = w * tf.reduce_sum(numerator, axis=(0, 1, 2, 3))
-    numerator = tf.reduce_sum(numerator)
+    numerator = w * tf.reduce_sum(numerator, axis=[1, 2])
+    numerator = tf.reduce_sum(numerator, axis=1)
 
     denominator = Y_pred + Y_gt
-    denominator = w * tf.reduce_sum(denominator, axis=(0, 1, 2, 3))
-    denominator = tf.reduce_sum(denominator)
+    denominator = w * tf.reduce_sum(denominator, axis=[1, 2])
+    denominator = tf.reduce_sum(denominator, axis=1)
 
     gen_dice_coef = 2 * numerator / (denominator + smooth)
-    loss = 1 - gen_dice_coef
+    loss = tf.reduce_mean(1 - gen_dice_coef)
     return loss
+
+
+def surface_loss_3d(Y_gt, Y_pred):
+    multipled = tf.reduce_sum(Y_gt * Y_pred, axis=[1, 2, 3, 4])
+    loss = tf.reduce_mean(multipled)
+    return loss
+
+
+def surface_loss_2d(Y_gt, Y_pred):
+    multipled = tf.reduce_sum(Y_gt * Y_pred, axis=[1, 2, 3])
+    loss = tf.reduce_mean(multipled)
+    return loss
+
+
+x = tf.constant(value=5., shape=(3, 32, 32, 32, 1))
+y = tf.constant(value=1., shape=(3, 32, 32, 32, 1))
+surface_loss_3d(x, y)
